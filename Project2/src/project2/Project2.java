d9bed9afe590726926c608577d557865a387f098
tolipbukankalengkaleng <89756191+tolipbukankalengkaleng@users.noreply.github.com>
2022-08-30 08:20:31 +0700

    Tugas 1
    Project 2

---
+++ Project2/src/project2/Project2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("RRRRRR     EEEEEEE      ZZZZZZZZ");
+        System.out.println("R    R     E                  Z");
+        System.out.println("R    R     E                 Z   ");
+        System.out.println("RRRRRR     EEEEEEE          Z     ");
+        System.out.println("RR         E              Z      ");
+        System.out.println("R  R       E             Z       ");
+        System.out.println("R    R     EEEEEEE      ZZZZZZZZZ");
+        
+    }
+    
+}
